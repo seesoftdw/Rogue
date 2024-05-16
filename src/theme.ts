875c1209fc436ae91af73174ded333b2b8f68c42
Harshit8011 <harshit1562002@gmail.com>
2024-05-16 11:57:51 -0400

    first commit

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main : "#f44336"
+        }
+    }
+})
+
+
+export default theme
